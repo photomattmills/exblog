35b139a1686e77b6435e8258b8849f0aaeb68fad
Matt Mills <sunrisetimes@gmail.com>
2020-04-04 02:16:45 -0700

    more config

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,12 +4,12 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+# database_url =
+#   System.get_env("DATABASE_URL") ||
+#     raise """
+#     environment variable DATABASE_URL is missing.
+#     For example: ecto://USER:PASS@HOST/DATABASE
+#     """
 
 config :exblog, Exblog.Repo,
     username: System.get_env("DATABASE_USER"),
@@ -31,7 +31,7 @@ config :exblog, ExblogWeb.Endpoint,
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base
+  secret_key_base: secret_key_base,
   server: true
 
 # ## Using releases (Elixir v1.9+)
