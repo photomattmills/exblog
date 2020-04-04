857f185be46c4b501afdfe4f08073fd2a86f8cff
Matt Mills <sunrisetimes@gmail.com>
2020-04-04 02:46:16 -0700

    small errata

--- lib/exblog/image_uploader.ex
+++ lib/exblog/image_uploader.ex
@@ -1,7 +1,6 @@
 defmodule Exblog.ImageUploader do
   def upload(path, object_key) do
     path
-    |> IO.inspect()
     |> ExAws.S3.Upload.stream_file()
     |> ExAws.S3.upload("mattdotpicturesimages", object_key,
       content_type: "image/jpeg",
