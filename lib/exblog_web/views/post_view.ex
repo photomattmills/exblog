1545a40bec0cc90c70d8f1377d9c7ef4968c88dd
Matt Mills <sunrisetimes@gmail.com>
2020-04-04 22:09:36 -0700

    add helper to escape post body in xml

--- lib/exblog_web/views/post_view.ex
+++ lib/exblog_web/views/post_view.ex
@@ -1,3 +1,16 @@
 defmodule ExblogWeb.PostView do
   use ExblogWeb, :view
+
+  def escape_xml(string) do
+    string
+    |> String.replace(">", "&gt;")
+    |> String.replace("<", "&lt;")
+    |> String.replace(~s|"|, "&quot;")
+    |> String.replace("'", "&apos;")
+    |> replace_ampersand
+  end
+
+  defp replace_ampersand(string) do
+    Regex.replace(~r/&(?!(lt|gt|quot|apos|amp);)/, string, "&amp;")
+  end
 end
