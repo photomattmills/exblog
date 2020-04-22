86056907429db0be3c7144465c9ea90737d0a35c
Matt Mills <sunrisetimes@gmail.com>
2020-04-21 22:32:20 -0700

    make the published checkbox better

--- lib/exblog_web/views/post_view.ex
+++ lib/exblog_web/views/post_view.ex
@@ -1,6 +1,9 @@
 defmodule ExblogWeb.PostView do
   use ExblogWeb, :view
 
+  def published(%{published_at: pa}) when pa != nil, do: true
+  def published(_post), do: false
+
   def escape_xml(string) do
     string
     |> String.replace(">", "&gt;")
