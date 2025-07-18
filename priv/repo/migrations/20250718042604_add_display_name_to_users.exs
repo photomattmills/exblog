cbb6a962229ed376341a5ab9de76433635f30c57
Matt Mills <sunrisetimes@gmail.com>
2025-07-17 21:59:31 -0700

    re-add user session layout

---
+++ priv/repo/migrations/20250718042604_add_display_name_to_users.exs
@@ -0,0 +1,9 @@
+defmodule Exblog.Repo.Migrations.AddDisplayNameToUsers do
+  use Ecto.Migration
+
+  def change do
+    alter table("users") do
+      add :display_name, :string
+    end
+  end
+end
